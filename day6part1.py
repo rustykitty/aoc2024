606290fc8c854c2a76cf17d2803b711f59e1a2f8
Rusty <rusty3925@gmail.com>
2024-12-05 21:30:10 -0800

    day 6 pt 1

---
+++ day6part1.py
@@ -0,0 +1,57 @@
+import sys
+labmap = [list(i) for i in sys.stdin.read().rstrip().split('\n')]
+pos = (-1, -1)
+
+# obstacles = set()
+
+rotation = 'up'
+
+def repeat(i):
+    while True:
+        for ele in i:
+            yield ele
+
+def get(pos):
+    x, y = pos
+    return labmap[x][y]
+
+for i in range(len(labmap)):
+    for j in range(len(labmap[0])):
+        # if labmap[i][j] == '#':
+        #     obstacles.add((i, j))
+        if labmap[i][j] == '^':
+            pos = (i, j)
+            labmap[i][j] = '.'
+
+assert all(x != -1 for x in pos)
+
+og_pos = pos
+
+gen = repeat([
+        (-1, 0), # up
+        (0, 1), # right
+        (1, 0), # down
+        (0, -1) # left
+])
+
+add = (lambda a, b: tuple(i + j for (i, j) in zip(a, b)))
+
+direction = next(gen)
+
+poses = set()
+
+while True:
+    fwd_p = add(pos, direction)
+    print(fwd_p)
+    if fwd_p[0] >= len(labmap[0]) or fwd_p[1] >= len(labmap) \
+        or fwd_p[0] < 0 or fwd_p[1] < 0:
+        direction = next(gen)
+        break
+    if get(fwd_p) == '#':
+        direction = next(gen)
+        continue
+    else:
+        poses.add(pos)
+        pos = add(direction, pos)
+
+print(len(poses) + 1)
